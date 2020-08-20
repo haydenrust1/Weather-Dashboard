3cc36b12819a71c45d1d231ae6043369c4264444
Haydenrust1 <haydenrust1@gmail.com>
2020-08-19 17:04:16 -0700

    adding ajax call to weather api

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 /****** API KEY: f462ebfd8a4c3783712546f3bca5bd3c ******/
 
 //method for running js only when DOM is ready
-$(document).ready(function() {
-
+$(document).ready(function () {
+  var weatherSearch = function (city) {
+    /* WRONG */
+    $.ajax({
+      url:
+        'api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&APPID=f462ebfd8a4c3783712546f3bca5bd3c',
+      method: 'GET',
+    }).then(function (response) {
+      console.log(response);
+    });
+  };
+  weatherSearch('portland');
 });
