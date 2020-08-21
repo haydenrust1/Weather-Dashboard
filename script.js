bfa446c1b27edf2d5973075c7a30279651fddd17
Haydenrust1 <haydenrust1@gmail.com>
2020-08-20 18:56:29 -0700

    added ajax call for weather app &&& added format for html (broken)

--- script.js
+++ script.js
@@ -3,16 +3,28 @@
 //method for running js only when DOM is ready
 $(document).ready(function () {
   var weatherSearch = function (city) {
-    /* WRONG */
+    // 5-day forecast call
     $.ajax({
       url:
-        'api.openweathermap.org/data/2.5/weather?q=' +
+        'https://api.openweathermap.org/data/2.5/forecast?q=' +
         city +
-        '&APPID=f462ebfd8a4c3783712546f3bca5bd3c',
+        '&units=imperial&APPID=f462ebfd8a4c3783712546f3bca5bd3c',
       method: 'GET',
     }).then(function (response) {
       console.log(response);
+      console.log(response.list[0].main.temp);
+    });
+    // Current day forecast call
+    $.ajax({
+      url:
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=imperial&APPID=f462ebfd8a4c3783712546f3bca5bd3c',
+      method: 'GET',
+    }).then(function (response) {
+      console.log(response);
+      console.log(response.main.temp);
     });
   };
-  weatherSearch('portland');
+  weatherSearch('Portland');
 });
